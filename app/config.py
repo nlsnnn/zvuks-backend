24f4d6829bbc874b6334f5f8e11e049bcfbb0563
nlsnnn <lagged.06@mail.ru>
2025-03-09 21:10:13 +0300

    refactor config

--- app/config.py
+++ app/config.py
@@ -1,35 +1,67 @@
 import os
 from pathlib import Path
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from app.services.s3 import S3Client
 
 
 BASE_DIR = Path(__file__).parent.parent
 
 
-class Settings(BaseSettings):
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_NAME: str
-    SECRET_KEY: str
-    ALGORITHM: str
+class DatabaseConfig(BaseModel):
+    host: str
+    port: int
+    user: str
+    password: str
+    name: str
 
+
+class JWTConfig(BaseModel):
+    secret_key: str
+    algorithm: str
+
+
+class S3Config(BaseModel):
+    access_key: str
+    secret_key: str
+    bucket_name: str
+    endpoint: str = "https://storage.yandexcloud.net"
+
+
+class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=os.path.join(BASE_DIR, ".env")
+        env_file=os.path.join(BASE_DIR, ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APPLICATION__",
     )
+
+    db: DatabaseConfig
+    jwt: JWTConfig
+    s3: S3Config
+
 
 settings = Settings()
 
 
 def get_db_url():
-    url = (f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@"
-            f"{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}")
+    url = (f"postgresql+asyncpg://{settings.db.user}:{settings.db.password}@"
+            f"{settings.db.host}:{settings.db.port}/{settings.db.name}")
     return url
 
 
 def get_auth_data():
-    return {"secret_key": settings.SECRET_KEY, "algorithm": settings.ALGORITHM}
+    return {"secret_key": settings.jwt.secret_key, "algorithm": settings.jwt.algorithm}
+
+
+def get_s3_client() -> S3Client:
+    return S3Client(
+        access_key=settings.s3.access_key,
+        secret_key=settings.s3.secret_key,
+        bucket_name=settings.s3.bucket_name,
+        endpoint_url=settings.s3.endpoint,
+    )
 
 
 def get_song_path():
