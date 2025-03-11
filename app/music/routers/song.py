8257424c2f21d20f6bdc26654e3485e479cfee4e
nlsnnn <egor.larrr@gmail.com>
2025-03-11 12:59:40 +0300

    update song add format paths

--- app/music/routers/song.py
+++ app/music/routers/song.py
@@ -26,10 +26,8 @@ async def get_song(song_id: int):
     if not song:
         raise HTTPException(status_code=404, detail="Song not found")
     
-    song_path = settings.s3.endpoint + "/" + settings.s3.bucket_name + "/" + song.path
-    cover_path = settings.s3.endpoint + "/" + settings.s3.bucket_name + "/" + song.cover_path
     
-    return {'song_path': song_path, 'cover_path': cover_path}
+    return {'song_path': song.path, 'cover_path': song.cover_path}
 
 
 @router.post("/")
@@ -45,10 +43,13 @@ async def add_song(
     song_data = await MusicService.save_song(song, cover, name, user_data.username)
     duration = MusicService.get_audio_duration(song.file)
 
+    song_path = settings.s3.endpoint + "/" + settings.s3.bucket_name + "/" + song_data['song_path']
+    cover_path = settings.s3.endpoint + "/" + settings.s3.bucket_name + "/" + song_data['cover_path']
+
     song_orm = await SongDAO.add(
         name=name,
-        path=song_data['song_path'],
-        cover_path=song_data['cover_path'],
+        path=song_path,
+        cover_path=cover_path,
         release_date=release_date_dt,
         duration=duration,
         user_id=user_data.id
