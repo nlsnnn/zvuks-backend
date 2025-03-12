854661a6e272e2dc607ad780f1dec155af0f5950
nlsnnn <egor.larrr@gmail.com>
2025-03-12 09:05:05 +0300

    fix websocket disconnect and optimization websocket request

--- app/chat/router.py
+++ app/chat/router.py
@@ -3,7 +3,6 @@ from typing import Dict
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends
 from app.chat.dao import MessagesDAO
 from app.chat.schemas import MessageCreate, MessageRead
-from app.users.dao import UsersDAO
 from app.users.dependencies import token_depends
 from app.users.models import User
 
@@ -17,7 +16,10 @@ active_connections: Dict[int, WebSocket] = {}
 async def notify_user(user_id: int, message: dict):
     if user_id in active_connections:
         websocket = active_connections[user_id]
-        await websocket.send_json(message)
+        try:
+            await websocket.send_json(message)
+        except WebSocketDisconnect:
+            active_connections.pop(user_id, None)
 
 
 @router.websocket("/ws/{user_id}")
@@ -28,6 +30,8 @@ async def websocket_endpoint(websocket: WebSocket, user_id: int):
         while True:
             await asyncio.sleep(1)
     except WebSocketDisconnect:
+        active_connections.pop(user_id, None)
+    finally:
         active_connections.pop(user_id, None)
 
 
@@ -52,7 +56,10 @@ async def send_message(message: MessageCreate, user_data: User = Depends(token_d
         'content': message.content,
         'created_at': str(message_orm.created_at)
     }
-    await notify_user(user_data.id, message_data)
-    await notify_user(message.recipient_id, message_data)
+
+    if user_data.id in active_connections:
+        await notify_user(user_data.id, message_data)
+    if message.recipient_id in active_connections:
+        await notify_user(message.recipient_id, message_data)
 
     return {'recipient_id': message.recipient_id, 'content': message.content, 'status': 'ok', 'msg': 'Message saved!'}
