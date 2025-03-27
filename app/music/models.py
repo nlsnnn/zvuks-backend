0b672523ccdbe414bed22cb7a9d1844a1b51be40
nlsnnn <lagged.06@mail.ru>
2025-03-27 11:01:13 +0300

    add song_artists table and update relationships in models

--- app/music/models.py
+++ app/music/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from sqlalchemy import ForeignKey, text
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base, int_pk
 from app.users.models import User
 
@@ -11,13 +11,11 @@ class Album(Base):
     cover_path: Mapped[str] = mapped_column(nullable=False)
     release_date: Mapped[datetime]
     is_archive: Mapped[bool] = mapped_column(
-        default=False, server_default=text('false')
+        default=False, server_default=text("false")
     )
-    
-    user_id: Mapped[User] = mapped_column(
-        ForeignKey('users.id', ondelete='CASCADE')
-    )
-    
+
+    user_id: Mapped[User] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+
 
 class Song(Base):
     id: Mapped[int_pk]
@@ -27,34 +25,35 @@ class Song(Base):
     release_date: Mapped[datetime] = mapped_column(nullable=True)
     track_number: Mapped[int] = mapped_column(nullable=True)
     is_archive: Mapped[bool] = mapped_column(
-        default=False, server_default=text('false')
+        default=False, server_default=text("false")
     )
 
     user_id: Mapped[User] = mapped_column(
-        ForeignKey('users.id', ondelete='CASCADE'),
-        nullable=True
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=True
     )
     album_id: Mapped[Album] = mapped_column(
-        ForeignKey('albums.id', ondelete='CASCADE'),
-        nullable=True
+        ForeignKey("albums.id", ondelete="CASCADE"), nullable=True
     )
+
+    artists: Mapped[list[User]] = relationship(
+        User, secondary="song_artists", back_populates="songs"
+    )
+
+
+class SongArtist(Base):
+    __tablename__ = "song_artists"
+
+    song_id: Mapped[int] = mapped_column(ForeignKey("songs.id", ondelete="CASCADE"), primary_key=True)
+    artist_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
 
 
 class UserAddSong(Base):
     id: Mapped[int_pk]
-    user_id: Mapped[User] = mapped_column(
-        ForeignKey('users.id', ondelete='CASCADE')
-    )
-    song_id: Mapped[Song] = mapped_column(
-        ForeignKey('songs.id', ondelete='CASCADE')
-    )
+    user_id: Mapped[User] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+    song_id: Mapped[Song] = mapped_column(ForeignKey("songs.id", ondelete="CASCADE"))
 
 
 class UserAddAlbum(Base):
     id: Mapped[int_pk]
-    user_id: Mapped[User] = mapped_column(
-        ForeignKey('users.id', ondelete='CASCADE')
-    )
-    album_id: Mapped[Album] = mapped_column(
-        ForeignKey('albums.id', ondelete='CASCADE')
-    )
+    user_id: Mapped[User] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+    album_id: Mapped[Album] = mapped_column(ForeignKey("albums.id", ondelete="CASCADE"))
--- app/users/models.py
+++ app/users/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import text
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base, str_uniq, int_pk
 
 
@@ -9,13 +9,20 @@ class User(Base):
     email: Mapped[str_uniq]
     password: Mapped[str]
 
-    is_user: Mapped[bool] = mapped_column(default=True, server_default=text('true'), nullable=False)
-    is_admin: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
-    is_super_admin: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
+    is_user: Mapped[bool] = mapped_column(
+        default=True, server_default=text("true"), nullable=False
+    )
+    is_admin: Mapped[bool] = mapped_column(
+        default=False, server_default=text("false"), nullable=False
+    )
+    is_super_admin: Mapped[bool] = mapped_column(
+        default=False, server_default=text("false"), nullable=False
+    )
 
+    songs: Mapped[list["Song"]] = relationship(
+        "Song", secondary="song_artists", back_populates="artists"
+    )
     extend_existing = True
 
     def __repr__(self):
         return f"{self.__class__.__name__}(id={self.id})"
-    
-
