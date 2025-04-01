b3ef2ce15e57db9806778b9734ddb1493d4eb68f
nlsnnn <egor.larrr@gmail.com>
2025-04-01 12:44:11 +0300

    add selectinload options in find method

--- app/music/song/dao.py
+++ app/music/song/dao.py
@@ -11,6 +11,23 @@ class SongDAO(BaseDAO):
     @classmethod
     async def find_all(cls, **filter_by):
         async with async_session_maker() as session:
-            query = select(cls.model).filter_by(**filter_by).options(selectinload(cls.model.artists))
+            query = (
+                select(cls.model)
+                .filter_by(**filter_by)
+                .options(selectinload(cls.model.artists))
+            )
             result = await session.execute(query)
             return result.scalars().all()
+
+    @classmethod
+    async def find_all_by_ids(cls, ids: list[int], **filter_by):
+        async with async_session_maker() as session:
+            query = (
+                select(cls.model)
+                .where(cls.model.id.in_(ids))
+                .filter_by(**filter_by)
+                .options(selectinload(cls.model.artists))
+            )
+            result = await session.execute(query)
+            songs = result.scalars().all()
+            return songs
