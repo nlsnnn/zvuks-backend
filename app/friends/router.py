1b679d86e30fe3ab34f6d20981fb5300a762375e
nlsnnn <lagged.06@mail.ru>
2025-05-19 23:40:26 +0300

    fix: update dependency injection for user data in friend request routes

--- app/friends/router.py
+++ app/friends/router.py
@@ -1,9 +1,7 @@
-from fastapi import APIRouter, Depends
-from typing import Annotated
+from fastapi import APIRouter
 
 from app.friends.schemas import SFriendRequest
 from app.friends.service import FriendRequestService
-from app.users.models import User
 from app.users.dependencies import CurrentUserDep
 
 
@@ -11,26 +9,26 @@ router = APIRouter(prefix="/friends", tags=["Friends"])
 
 
 @router.get("/")
-async def get_friends(user_data: Annotated[User, Depends(CurrentUserDep)]):
+async def get_friends(user_data: CurrentUserDep):
     friends = await FriendRequestService.get_friends(user_data.id)
     return {"friends": friends}
 
 
 @router.get("/pending")
-async def get_pending_requests(user_data: Annotated[User, Depends(CurrentUserDep)]):
+async def get_pending_requests(user_data: CurrentUserDep):
     requests = await FriendRequestService.get_pending_requests(user_data.id)
     return {"requests": requests}
 
 
 @router.get("/sended")
-async def get_sended_requests(user_data: Annotated[User, Depends(CurrentUserDep)]):
+async def get_sended_requests(user_data: CurrentUserDep):
     requests = await FriendRequestService.get_sended_requests(user_data.id)
     return {"requests": requests}
 
 
 @router.post("/")
 async def send_friend_request(
-    data: SFriendRequest, user_data: Annotated[User, Depends(CurrentUserDep)]
+    data: SFriendRequest, user_data: CurrentUserDep
 ):
     await FriendRequestService.send_friend_request(
         user_sended_id=user_data.id, user_received_id=data.user_received_id
@@ -41,7 +39,7 @@ async def send_friend_request(
 
 @router.post("/accept")
 async def accept_friend_request(
-    request: SFriendRequest, user_data: Annotated[User, Depends(CurrentUserDep)]
+    request: SFriendRequest, user_data: CurrentUserDep
 ):
     await FriendRequestService.accept_friend_request(
         request.user_sended_id, user_data.id
@@ -51,7 +49,7 @@ async def accept_friend_request(
 
 @router.post("/reject")
 async def reject_friend_request(
-    request: SFriendRequest, user_data: Annotated[User, Depends(CurrentUserDep)]
+    request: SFriendRequest, user_data: CurrentUserDep
 ):
     await FriendRequestService.reject_friend_request(
         user_sended_id=request.user_sended_id, user_received_id=user_data.id
@@ -61,7 +59,7 @@ async def reject_friend_request(
 
 @router.post("/delete")
 async def delete_friend(
-    request: SFriendRequest, user_data: Annotated[User, Depends(CurrentUserDep)]
+    request: SFriendRequest, user_data: CurrentUserDep
 ):
     await FriendRequestService.delete_friend(request.user_id, user_data.id)
     return {"message": "Друг удален"}
